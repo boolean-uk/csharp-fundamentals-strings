4e9a1e495307798369b3795b091db2014e145b3e
Thanasis Andritsios <thanasis1997abc@gmail.com>
2023-04-27 15:00:00 +0300

    done with core

--- csharp-fundamentals-strings.Main/Program.cs
+++ csharp-fundamentals-strings.Main/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using csharp_fundamentals_strings.Main;
+
 Console.WriteLine("Hello, World!");
+
+Core core = new Core();
+
+Console.WriteLine(core.faqUrl);
