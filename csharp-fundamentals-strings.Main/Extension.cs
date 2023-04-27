553d14d727e352715979748c767935d841bf2dbc
Nikita Pieters <nikita_pieters@hotmail.com>
2023-04-27 17:15:18 +0200

    finished the second extension

--- csharp-fundamentals-strings.Main/Extension.cs
+++ csharp-fundamentals-strings.Main/Extension.cs
@@ -57,8 +57,10 @@ namespace csharp_fundamentals_strings.Main
             // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
             // 2. After adding the message, use an appropriate StringBuilder method to reverse it
             // WRITE YOUR CODE BETWEEN THIS LINE...
-           
-
+            string hello = "Hello, world!";
+            char[] str = hello.ToString().ToCharArray();
+            Array.Reverse(str);
+            sb.Append(str);
 
             // ...AND THIS LINE
 
