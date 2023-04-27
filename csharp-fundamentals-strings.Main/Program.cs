e43bdcf80cca8698665f792b99854f79ea175d20
StavrosSpil <spiliopoulos.stavros@hotmail.com>
2023-04-27 17:59:56 +0300

    Standard and extensions completed

--- csharp-fundamentals-strings.Main/Program.cs
+++ csharp-fundamentals-strings.Main/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using csharp_fundamentals_strings.Main;
+
 Console.WriteLine("Hello, World!");
+
+Core core = new Core();
+Console.WriteLine(core.domain);
