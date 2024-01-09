32677de788dd6b59e22f4edfa3978075ad612e3e
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-01-09 13:35:47 +0100

    Completed main and Extension tasks.

--- csharp-fundamentals-strings.Main/Extension.cs
+++ csharp-fundamentals-strings.Main/Extension.cs
@@ -42,9 +42,7 @@ namespace csharp_fundamentals_strings.Main
 
             // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
             // WRITE YOUR CODE BETWEEN THIS LINE...
-
-
-
+            sb.Append("Hello, world!");
             // ...AND THIS LINE
 
             return sb;
@@ -57,9 +55,14 @@ namespace csharp_fundamentals_strings.Main
             // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
             // 2. After adding the message, use an appropriate StringBuilder method to reverse it
             // WRITE YOUR CODE BETWEEN THIS LINE...
+            sb.Append("Hello, world!");
 
-
-
+            for (int i = 0; i < sb.Length /2; i++)
+            {
+                char temp = sb[i];
+                sb[i] = sb[sb.Length - 1 -i];
+                sb[sb.Length - 1 - i] = temp;
+            }
             // ...AND THIS LINE
 
             return sb;
@@ -72,9 +75,8 @@ namespace csharp_fundamentals_strings.Main
             // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
             // 2. After adding the message, remove the comma.
             // WRITE YOUR CODE BETWEEN THIS LINE...
-
-
-
+            sb.Append("Hello, world!");
+            sb.Remove(sb.ToString().IndexOf(','), 1);
             // ...AND THIS LINE
 
             return sb;
@@ -87,8 +89,8 @@ namespace csharp_fundamentals_strings.Main
             // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
             // 2. After adding the message, replace the word "world" with the word "C#"
             // WRITE YOUR CODE BETWEEN THIS LINE...
-
-
+            sb.Append("Hello, world!");
+            sb.Replace("world", "C#");
 
             // ...AND THIS LINE
 
