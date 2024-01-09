0db4ff0f26fcba060a63e86cb7298be03f7bd22d
Oneal <oneallane2808@gmail.com>
2024-01-09 14:42:34 +0100

    solved, removed code from program.cs

--- csharp-fundamentals-strings.Main/Program.cs
+++ csharp-fundamentals-strings.Main/Program.cs
@@ -2,18 +2,3 @@
 using System.Text;
 
 Console.WriteLine("Hello, World!");
-
-
-StringBuilder sb = new StringBuilder();
-string str = sb.ToString();
-
-
- static string reverse1(string str)
-{
-    char[] charArr = str.ToCharArray();
-    Array.Reverse(charArr);
-    return new string(charArr);
-}
-
-string newstr = reverse1(str);
-Console.WriteLine(newstr);
