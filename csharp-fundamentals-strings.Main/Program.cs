83e18ae49042f55144385976f033a3638861e547
Oneal <oneallane2808@gmail.com>
2024-01-09 14:31:52 +0100

    solved

--- csharp-fundamentals-strings.Main/Program.cs
+++ csharp-fundamentals-strings.Main/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
 Console.WriteLine("Hello, World!");
+
+
+StringBuilder sb = new StringBuilder();
+string str = sb.ToString();
+
+
+ static string reverse1(string str)
+{
+    char[] charArr = str.ToCharArray();
+    Array.Reverse(charArr);
+    return new string(charArr);
+}
+
+string newstr = reverse1(str);
+Console.WriteLine(newstr);
