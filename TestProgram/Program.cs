72f0638ff5361681870ca56d9ebc23cfd69d126f
Aziz Zafar <aziz.zafar@no.experis.com>
2024-02-02 23:09:42 +0100

    Updated csharp-fundamentals-strings

--- TestProgram/Program.cs
+++ TestProgram/Program.cs
@@ -1,14 +1,2 @@
-﻿using System.Text;
-StringBuilder sb = new StringBuilder();
-
-// 1. Add "Hello, world!" to the StringBuilder
-sb.Append("Hello, world!");
-
-// 2. Convert StringBuilder content to a string, reverse it, and update the StringBuilder
-string reversedString = sb.ToString();
-char[] charArray = reversedString.ToCharArray();
-Array.Reverse(charArray);
-sb.Clear();
-sb.Append(charArray);
-
-Console.WriteLine(sb);
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
