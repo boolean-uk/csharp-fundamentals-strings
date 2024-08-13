43f6e2ff70d364ab087d58f42ca4a839ec8bd729
Thomas Aleksander Flier <thomas.flier@no.experis.com>
2024-08-13 08:33:59 +0200

    cranky

--- csharp-fundamentals-strings.Main/Extension.cs
+++ csharp-fundamentals-strings.Main/Extension.cs
@@ -43,7 +43,7 @@ namespace csharp_fundamentals_strings.Main
             // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
             // WRITE YOUR CODE BETWEEN THIS LINE...
 
-
+            sb.Append("Hello, world!");
 
             // ...AND THIS LINE
 
@@ -57,8 +57,12 @@ namespace csharp_fundamentals_strings.Main
             // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
             // 2. After adding the message, use an appropriate StringBuilder method to reverse it
             // WRITE YOUR CODE BETWEEN THIS LINE...
-
-
+            sb.Append("Hello, world!");
+            char[] reverso = sb.ToString().ToCharArray();
+            Array.Reverse(reverso);
+            string s = new String(reverso);
+            sb.Replace("Hello, world!", s);
+            
 
             // ...AND THIS LINE
 
