4ed0533f2ea152ffb1b90fc7a2603ca36ae15626
Dennis Rizah Nordvi Osmani <dennis.osmani@no.experis.com>
2024-08-13 08:08:20 +0200

    Core done

--- csharp-fundamentals-strings.Main/Extension.cs
+++ csharp-fundamentals-strings.Main/Extension.cs
@@ -42,7 +42,7 @@ namespace csharp_fundamentals_strings.Main
 
             // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
             // WRITE YOUR CODE BETWEEN THIS LINE...
-
+            sb.Append("Hello, world!");
 
 
             // ...AND THIS LINE
@@ -58,7 +58,11 @@ namespace csharp_fundamentals_strings.Main
             // 2. After adding the message, use an appropriate StringBuilder method to reverse it
             // WRITE YOUR CODE BETWEEN THIS LINE...
 
+            sb.Append("Hello, world!");
+    
 
+           // Console.WriteLine(sb.ToString());
+  
 
             // ...AND THIS LINE
 
